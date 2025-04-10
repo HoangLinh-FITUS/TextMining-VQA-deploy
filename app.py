ce306820ac35a75a10da995f4d12b3371d6f2957
thlinh22 <thlinh22@clc.fitus.edu.vn>
2025-04-10 08:18:47 +0700

    update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 page1 = st.Page("check.py", title='Check Test Data')
 page2 = st.Page("models.py", title='Models')
-pg = st.navigation([page1, page2])
+# pg = st.navigation([page1, page2])
+pg = st.navigation([page2])
 
 pg.run()
